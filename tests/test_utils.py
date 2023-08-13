5db4201cde6296df906b6427bb2c7f1f41e502cf
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-08-14 01:06:05 +0300

    changed def test_load_json

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
-from utils.utils import load_json
+from course_work_3.utils.utils import load_json
 
 
 def test_load_json():
-    assert load_json('data/test_json.json') == [{"id": 441945886, "state": "EXECUTED"},
+    assert load_json('course_work_3/data/test_json.json') == [{"id": 441945886, "state": "EXECUTED"},
                                                 {"id": 41428829, "state": "EXECUTED"}]
