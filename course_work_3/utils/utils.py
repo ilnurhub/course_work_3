2929973409702c8d4646114b627d834172ff7e0b
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-08-14 01:08:28 +0300

    created def sort_list_of_dicts

--- course_work_3/utils/utils.py
+++ course_work_3/utils/utils.py
@@ -8,3 +8,11 @@ def load_json(file):
     with open(file, 'r', encoding='utf-8') as f:
         lst = json.load(f)
     return lst
+
+
+def sort_list_of_dicts(user_list, sort_key, rev=True):
+    """
+    Сортирует список словарей по заданному ключу
+    """
+    sorted_user_list = sorted(user_list, key=lambda d: d.get(sort_key, '0'), reverse=rev)
+    return sorted_user_list
