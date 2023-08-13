5d53b0600662a92f4b55d573d1c27d93f2fa2242
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-08-14 01:10:40 +0300

    created def test_title_and_account

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,4 @@
-from course_work_3.utils.utils import load_json, sort_list_of_dicts
+from course_work_3.utils.utils import load_json, sort_list_of_dicts, title_and_account
 
 
 def test_load_json():
@@ -9,3 +9,7 @@ def test_load_json():
 def test_sort_list_of_dicts():
     assert sort_list_of_dicts([{'id': 1, 'date': '2009-12-08'}, {'id': 2, 'date': '2023-06-15'}], 'date') == \
            [{'id': 2, 'date': '2023-06-15'}, {'id': 1, 'date': '2009-12-08'}]
+
+
+def test_title_and_account():
+    assert title_and_account('Visa Classic 6831982476737658') == ('Visa Classic', '6831982476737658')
