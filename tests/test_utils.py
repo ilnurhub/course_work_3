ae500d541e077bbef93275742d5285e3e9f48903
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-08-14 01:07:27 +0300

    created def test_sort_list_of_dicts

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,11 @@
-from course_work_3.utils.utils import load_json
+from course_work_3.utils.utils import load_json, sort_list_of_dicts
 
 
 def test_load_json():
     assert load_json('course_work_3/data/test_json.json') == [{"id": 441945886, "state": "EXECUTED"},
-                                                {"id": 41428829, "state": "EXECUTED"}]
+                                                              {"id": 41428829, "state": "EXECUTED"}]
+
+
+def test_sort_list_of_dicts():
+    assert sort_list_of_dicts([{'id': 1, 'date': '2009-12-08'}, {'id': 2, 'date': '2023-06-15'}], 'date') == \
+           [{'id': 2, 'date': '2023-06-15'}, {'id': 1, 'date': '2009-12-08'}]
